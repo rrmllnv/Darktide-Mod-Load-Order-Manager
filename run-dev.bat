d94741ab879e02a0b164376f22b56f470ba44f18
Rifat Mullayanov <34332137+rifat0mu@users.noreply.github.com>
2026-01-17 00:57:26 +0300

    Update title in index.html

--- run-dev.bat
+++ run-dev.bat
@@ -1,7 +1,7 @@
 @echo off
 chcp 65001 >nul 2>&1
 cd /d "%~dp0"
-echo Starting Mod Load Order Manager (JavaScript) in DEV mode...
+echo Starting Darktide Mod Load Order Manager (JavaScript) in DEV mode...
 echo DevTools will be opened automatically.
 echo.
 
